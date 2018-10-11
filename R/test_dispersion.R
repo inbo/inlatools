6b0572edb6a76b016a45570b519da0aef7e689ff
ThierryO <thierry.onkelinx@inbo.be>
2018-10-11 10:28:20 +0200

    convert test_dispersion into an S4 method

--- R/test_dispersion.R
+++ R/test_dispersion.R
@@ -2,60 +2,77 @@
 #' @inheritParams get_observed
 #' @param nsim the number of simulation
 #' @param plot display a plot of of the simulated values
-#' @export
+#' @name test_dispersion
+#' @rdname test_dispersion
+#' @exportMethod test_dispersion
+#' @docType methods
+#' @importFrom methods setGeneric
+setGeneric(
+  name = "test_dispersion",
+  def = function(object, nsim = 1000, plot = TRUE){
+    standardGeneric("test_dispersion") # nocov
+  }
+)
+
+#' @rdname test_dispersion
+#' @importFrom methods setMethod new
 #' @importFrom assertthat assert_that is.flag is.count
 #' @importFrom INLA inla.posterior.sample
 #' @importFrom purrr map_dfc
 #' @importFrom ggplot2 ggplot aes geom_density geom_vline ggtitle
-test_dispersion <- function(object, nsim = 1000, plot = TRUE) {
-  assert_that(inherits(object, "inla"))
-  assert_that(is.count(nsim))
-  assert_that(is.flag(plot))
+#' @include s3_classes.R
+setMethod(
+  f = "test_dispersion",
+  signature = signature(object = "inla"),
+  definition = function(object, nsim = 1000, plot = TRUE) {
+    assert_that(is.count(nsim))
+    assert_that(is.flag(plot))
 
-  if (length(object$.args$family) > 1) {
-    stop("Only single responses are handled")
-  }
+    if (length(object$.args$family) > 1) {
+      stop("Only single responses are handled")
+    }
 
-  observed <- get_observed(object)
-  fitted <- fitted(object)
-  samples <- inla.posterior.sample(n = nsim, result = object) #nolint
-  relevant <- grep("^Predictor:", rownames(samples[[1]]$latent))
-  eta <- map_dfc(samples, "latent")[relevant, ]
-  if (object$.args$family == "poisson") {
-    dispersion_data <- dispersion(
-      observed = observed,
-      fitted = fitted,
-      variance = fitted
-    )
-    dispersion_model <- apply(
-      exp(eta),
-      2,
-      function(x) {
-        dispersion(observed = observed, fitted = x, variance = x)
-      }
-    )
-  } else {
-    stop(object$.args$family, " is not yet handled")
-  }
-
-  if (!isTRUE(plot)) {
-    return(invisible(list(data = dispersion_data, model = dispersion_model)))
-  }
-
-  p <- ggplot(data.frame(dispersion = dispersion_model), aes(x = dispersion)) +
-    geom_density() +
-    geom_vline(xintercept = dispersion_data, linetype = 2) +
-    ggtitle(
-      paste(
-        "P(D|data > D|model) =",
-        signif(mean(dispersion_data > dispersion_model), 3)
+    observed <- get_observed(object)
+    fitted <- fitted(object)
+    samples <- inla.posterior.sample(n = nsim, result = object) #nolint
+    relevant <- grep("^Predictor:", rownames(samples[[1]]$latent))
+    eta <- map_dfc(samples, "latent")[relevant, ]
+    if (object$.args$family == "poisson") {
+      dispersion_data <- dispersion(
+        observed = observed,
+        fitted = fitted,
+        variance = fitted
       )
+      dispersion_model <- apply(
+        exp(eta),
+        2,
+        function(x) {
+          dispersion(observed = observed, fitted = x, variance = x)
+        }
+      )
+    } else {
+      stop(object$.args$family, " is not yet handled")
+    }
+
+    if (!isTRUE(plot)) {
+      return(invisible(list(data = dispersion_data, model = dispersion_model)))
+    }
+
+    p <- ggplot(data.frame(dispersion = dispersion_model), aes(x = dispersion)) +
+      geom_density() +
+      geom_vline(xintercept = dispersion_data, linetype = 2) +
+      ggtitle(
+        paste(
+          "P(D|data > D|model) =",
+          signif(mean(dispersion_data > dispersion_model), 3)
+        )
+      )
+    print(p)
+    return(
+      invisible(list(data = dispersion_data, model = dispersion_model, plot = p))
     )
-  print(p)
-  return(
-    invisible(list(data = dispersion_data, model = dispersion_model, plot = p))
-  )
-}
+  }
+)
 
 #' Calculate a measure for dispersion
 #' The measure is calculated as the sum of the squared Pearson residuals
