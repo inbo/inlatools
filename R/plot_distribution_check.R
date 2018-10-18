188f1fe7fa38fc33b5f05a7c45f8dfa35b069938
ThierryO <thierry.onkelinx@inbo.be>
2018-10-18 22:02:43 +0200

    plot.distribution() displays at least the 0.95 - 1.05 interval

--- R/plot_distribution_check.R
+++ R/plot_distribution_check.R
@@ -6,9 +6,10 @@
 #' @return a ggplot2 object
 #' @importFrom assertthat assert_that has_name
 #' @importFrom dplyr %>% mutate filter
-#' @importFrom ggplot2 ggplot aes_string geom_ribbon geom_line geom_hline ylab
-#' geom_text
+#' @importFrom ggplot2 ggplot aes_string geom_ribbon geom_line geom_hline
+#' geom_text geom_blank scale_y_continuous
 #' @importFrom graphics plot
+#' @importFrom scales percent
 #' @export
 plot.distribution_check <- function(x, y, ...) {
   assert_that(
@@ -27,12 +28,13 @@ plot.distribution_check <- function(x, y, ...) {
       lcl = .data$ecdf / .data$lcl,
       ucl = .data$ecdf / .data$ucl
     ) %>%
-    ggplot(aes_string(x = "x", y = "median", ymin = "lcl", ymax = "ucl")) +
+    ggplot(aes_string(x = "x", y = "median")) +
+    geom_blank(data = data.frame(x = min(x$x), median = c(0.95, 1.05))) +
     geom_hline(yintercept = 1, linetype = 2) +
     geom_line(aes_string(y = "lcl"), linetype = 3, alpha = 0.5) +
     geom_line(aes_string(y = "ucl"), linetype = 3, alpha = 0.5) +
-    geom_ribbon(alpha = 0.1) +
+    geom_ribbon(alpha = 0.1, aes_string(ymin = "lcl", ymax = "ucl")) +
     geom_line() +
     geom_text(aes_string(label = "n"), angle = 90, hjust = 1.5) +
-    ylab("observed / expected")
+    scale_y_continuous("observed / expected", labels = percent)
 }
