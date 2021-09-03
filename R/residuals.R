0db17bd4e43c3bffd98ba127d0bc7fa721ec2a4c
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-09-03 11:12:55 +0200

    residuals() handles zero inflated poisson and zero inflated negative binomial

--- R/residuals.R
+++ R/residuals.R
@@ -38,15 +38,28 @@ setMethod(
       msg = "Only single responses are handled"
     )
     assert_that(
-      object$.args$family %in% c("nbinomial", "poisson"),
-      msg = paste(object$.args$family, "distribution not handled")
+      object$.args$family %in% c(
+        "nbinomial", "poisson", "zeroinflatednbinomial1", "zeroinflatedpoisson1"
+      ),
+      msg = sprintf(
+        "`%s` distribution not (yet) handled by `residuals()`.
+Please open an issue at https://github.com/inbo/inlatools.",
+        object$.args$family
+      )
     )
     observed <- get_observed(object)
     fitted <- fitted(object)
     variance <- switch(
       object$.args$family,
-      nbinomial = fitted + (1 + fitted / get_mean_size(object)),
-      poisson = fitted
+      nbinomial = var_nbinom(mu = fitted, size = get_mean_size(object)),
+      poisson = fitted,
+      zeroinflatednbinomial1 = var_zinbinom1(
+        mu = fitted, size = get_mean_size(object),
+        zero = get_mean_zero_prob(object)
+      ),
+      zeroinflatedpoisson1 = var_zipois1(
+        mu = fitted, zero = get_mean_zero_prob(object)
+      )
     )
     (observed - fitted) / sqrt(variance)
   }
@@ -55,11 +68,64 @@ setMethod(
 #' @importFrom assertthat assert_that
 get_mean_size <- function(object) {
   which_size <- grep(
-    "size for the nbinomial observations",
+    "size for .*nbinomial.*observations",
     rownames(object$summary.hyperpar)
   )
   assert_that(
-    length(which_size) == 1, msg = "Can't find size for nbinomial observations"
+    length(which_size) == 1, msg = "Can't find size for nbinomial observations."
   )
   object$summary.hyperpar$mean[which_size]
+}
+
+#' Variance of the negative binomial distribution
+#' @param mu mean of the distribution.
+#' Must be on the original scale.
+#' @param size Size of the negative binomial distribution.
+#' Must be strict positive.
+#' @rdname variance
+#' @family variance
+#' @export
+var_nbinom <- function(mu, size) {
+  assert_that(all(mu > 0), all(size > 0))
+  mu * (1 + mu / size)
+}
+
+#' Variance of the zero-inflated negative binomial distribution type 1
+#'
+#' The type 1 zero-inflated negative binomial distribution is a standard
+#' negative binomial distribution with an additional point mass at zero.
+#' @inheritParams var_nbinom
+#' @inheritParams var_zipois1
+#' @rdname variance
+#' @family variance
+#' @export
+var_zinbinom1 <- function(mu, size, zero) {
+  assert_that(all(mu > 0), all(size > 0), all(zero > 0), all(zero < 1))
+  mu * (1 + mu * (zero + 1 / size) / (1 - zero))
+}
+
+#' Variance of the zero-inflated poisson distribution type 1
+#'
+#' The type 1 zero-inflated poisson is a standard Poisson distribution with an
+#' additional point mass at zero.
+#' @inheritParams var_nbinom
+#' @param zero Probability of the point mass at zero.
+#' @rdname variance
+#' @family variance
+#' @export
+var_zipois1 <- function(mu, zero) {
+  assert_that(all(mu > 0), all(zero > 0), all(zero < 1))
+  mu * (1 + mu * zero / (1 - zero))
+}
+
+#' @importFrom assertthat assert_that
+get_mean_zero_prob <- function(object) {
+  which_prob <- grep(
+    "zero-probability",
+    rownames(object$summary.hyperpar)
+  )
+  assert_that(
+    length(which_prob) == 1, msg = "Can't find zero-probability parameter."
+  )
+  object$summary.hyperpar$mean[which_prob]
 }
